73d6676aabf831cb9d807bf545b64f43f3131bc2
Bryce Willey <bryce.steven.willey@gmail.com>
2016-08-27 20:00:08 -0500

    Moved over first actual project

---
+++ lib/app_component.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/core.dart';
+@Component(
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>')
+class AppComponent {}
+
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:personal_website/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
