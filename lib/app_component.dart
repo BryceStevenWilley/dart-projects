641afadfdd55cde9dc92e8d63d88d848a8a66100
Bryce Willey <bryce.steven.willey@gmail.com>
2016-08-27 22:30:43 -0500

    Added a message about being under construction

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,9 @@
 import 'package:angular2/core.dart';
+import 'package:personal_website/site_header.dart';
+
 @Component(
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>')
+    templateUrl: 'app_component.html',
+    directives: const [SiteHeader])
 class AppComponent {}
 
---
+++ lib/site_header.dart
@@ -0,0 +1,5 @@
+import 'package:angular2/core.dart';
+@Component(
+    selector: 'site-header',
+    templateUrl: 'site_header.html')
+class SiteHeader {}
