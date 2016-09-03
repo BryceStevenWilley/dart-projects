7490027be5e235fcd462594eeadab414bad61aa7
Bryce Willey <bryce.steven.willey@gmail.com>
2016-09-03 00:57:08 -0500

    Added the push script

---
+++ push_website.sh
@@ -0,0 +1,6 @@
+pub build
+cd build/web
+zip -r webapp.zip *
+scp webapp.zip bsw2@ssh.clear.rice.edu:~/Public/
+ssh bsw2@ssh.clear.rice.edu 'cd Public/www/; rm * -rf; mv ../webapp.zip .; unzip webapp.zip; rm webapp.zip'
+
