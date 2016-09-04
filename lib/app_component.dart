d54277a7ea545ec07079b34d482aae02dde63bb4
Bryce Willey <bryce.steven.willey@gmail.com>
2016-09-04 03:30:40 -0500

    Disguisting circle web app implementation. Clean up before starting Galaga.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,24 @@
 import 'package:angular2/core.dart';
-import 'package:personal_website/site_header.dart';
+import 'package:angular2/router.dart';
+import 'package:personal_website/home_component.dart';
+import 'package:personal_website/circle_drawer_component.dart';
 
 @Component(
     selector: 'my-app',
     templateUrl: 'app_component.html',
-    directives: const [SiteHeader])
+    directives: const [ROUTER_DIRECTIVES])
+@RouteConfig(const [
+  const Route(
+      path: '/home',
+      name: 'Home',
+      component: HomeComponent,
+      useAsDefault: true
+  ),
+  const Route(
+      path: '/web-apps',
+      name: "WebApps",
+      component: CircleDrawerComponent
+  )
+])
 class AppComponent {}
 
