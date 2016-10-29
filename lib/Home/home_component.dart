b1e20582362d837c058dc621c9aae507ba4dfbb7
Bryce Willey <bryce.steven.willey@gmail.com>
2016-10-28 20:34:19 -0500

    Cleaned up the home component.

--- lib/Home/home_component.dart
+++ lib/Home/home_component.dart
@@ -1,4 +1,7 @@
 import 'package:angular2/core.dart';
 
-@Component(selector: 'home-component', templateUrl: 'home_component.html')
+@Component(
+    selector: 'home-component',
+    templateUrl: 'home_component.html',
+    styleUrls: const ["home_component.css"])
 class HomeComponent {}
