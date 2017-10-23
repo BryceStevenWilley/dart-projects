21fd7063796e8880bd54dc96d2da7e5d9e9d0656
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2017-10-22 23:05:03 -0500

    Moved pics to new website, removed Home page and forest page from old site.

--- lib/AppComponent/app_component.dart
+++ lib/AppComponent/app_component.dart
@@ -1,9 +1,7 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-import 'package:personal_website/Home/home_component.dart';
 import 'package:personal_website/CircleDrawer/circle_drawer_component.dart';
 import 'package:personal_website/Logo/logo_component.dart';
-import 'package:personal_website/OculusHack/oculus_hack_component.dart';
 
 @Component(
     selector: 'my-app',
@@ -12,15 +10,8 @@ import 'package:personal_website/OculusHack/oculus_hack_component.dart';
     directives: const [ROUTER_DIRECTIVES])
 @RouteConfig(const [
   const Route(
-      path: '/home',
-      name: 'Home',
-      component: HomeComponent,
-      useAsDefault: true),
-  const Route(
-      path: '/circles', name: 'Circles', component: CircleDrawerComponent),
+      path: '/circles', name: 'Circles', component: CircleDrawerComponent, useAsDefault: true),
   const Route(path: '/logo', name: 'Logo', component: LogoComponent),
-  const Route(
-      path: '/ovr-hr', name: 'OculusHackRice', component: OculusHackComponent),
 ])
 class AppComponent {
   Router router;
